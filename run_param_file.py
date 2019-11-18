9ce4511f4ddfdebd6c0dee05b61aa682df5adca6
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-18 21:47:44 +0000

    pi/3 Ta 5e2

--- run_param_file.py
+++ run_param_file.py
@@ -10,8 +10,8 @@ Pr = 1.                             # Prandtl number
 Pm = 1.                             # Magnetic Prandtl number
 Ra = 4e5                         # Rayleigh number
 Np = 0
-Ta = 5e4
-latitude = np.pi / 4                      # Number of density scale heights
+Ta = 5e2
+latitude = np.pi / 3                      # Number of density scale heights
 m = 1.5                             # Polytropic index
 theta = 1 - np.exp(-Np/m)           # Dimensionaless inverse T scale height
 
