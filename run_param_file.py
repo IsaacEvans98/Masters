10b29917aabce6f7ffffdbc75a15a55dfe99f0ae
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-19 09:38:43 +0000

    Ra4e4 Np0.1 Ta1e5 lat pi/4

--- run_param_file.py
+++ run_param_file.py
@@ -8,10 +8,10 @@ Lx, Lz = 2, 1                       # Domain size
 Nx, Nz = 128, 64                    # Number of
 Pr = 1.                             # Prandtl number
 Pm = 1.                             # Magnetic Prandtl number
-Ra = 4e5                         # Rayleigh number
-Np = 0
-Ta = 5e2
-latitude = np.pi / 3                      # Number of density scale heights
+Ra = 4e4                         # Rayleigh number
+Np = 0.1
+Ta = 1e5
+latitude = np.pi / 4                     # Number of density scale heights
 m = 1.5                             # Polytropic index
 theta = 1 - np.exp(-Np/m)           # Dimensionaless inverse T scale height
 
@@ -23,4 +23,4 @@ analysis_freq = 1.5e-4              # Frequency analysis files are outputted
 
 end_sim_time = 2.                   # Stop time in simulations units
 end_wall_time = np.inf              # Stop time in wall time
-end_iterations = np.inf             # Stop time in iterations
+end_iterations = np.inf             # Stop time in iterations I'm a nonce
