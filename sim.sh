a9542f3401af447e11ffc6f9558434508c6f94af
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-20 07:28:15 +0000

    15 cores

--- sim.sh
+++ sim.sh
@@ -3,6 +3,6 @@
 source /opt/Miniconda/miniconda37/etc/profile.d/conda.sh
 conda activate dedalus
 
-mpiexec -n 10  python3 anelastic_RB_fplane.py
+mpiexec -n 15  python3 anelastic_RB_fplane.py
 
 exit
