3e97f81869e42df2f77ad4b2fb13f241255b7634
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-24 19:52:56 +0000

    avg start 0.5 avg stop 1.9

--- plotting_snapshots3.py
+++ plotting_snapshots3.py
@@ -23,8 +23,8 @@ plot_snapshots = True
 # Best to run the script once first with plot_fluxes = False, and checking the
 # KE plot to see when the simulation has equilibrated, then running again with
 # plot_fluxes = True, and sensible values for the two parameters below.
-avg_t_start = 0.2
-avg_t_stop  = 0.9
+avg_t_start = 0.5
+avg_t_stop  = 1.9
 
 if os.path.exists(save_direc) == False:
     pathlib.Path(save_direc).mkdir(parents=True)
