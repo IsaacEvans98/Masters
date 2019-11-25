ecf247db4ee1d31a45db088d074c274f7e9b971e
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-25 15:55:18 +0000

    new freqs

--- run_param_file.py
+++ run_param_file.py
@@ -18,8 +18,8 @@ theta = 1 - np.exp(-Np/m)           # Dimensionaless inverse T scale height
 initial_timestep = 2e-5                 # Initial timestep
 max_dt = 1e-4                         # max dt
 
-snapshot_freq = 1.5e-3              # Frequency snapshot files are outputted
-analysis_freq = 1.5e-4              # Frequency analysis files are outputted
+snapshot_freq = 1.5e-4              # Frequency snapshot files are outputted
+analysis_freq = 1.5e-5              # Frequency analysis files are outputted
 
 end_sim_time = 2.                   # Stop time in simulations units
 end_wall_time = np.inf              # Stop time in wall time
