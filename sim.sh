d6caa8958dab6650a7e01c70438ca8679ab1f411
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-27 15:39:48 +0000

    5 cores

--- sim.sh
+++ sim.sh
@@ -3,7 +3,7 @@
 source /opt/Miniconda/miniconda37/etc/profile.d/conda.sh
 conda activate dedalus
 
-mpiexec -n 4 python3 anelastic_RB_fplane.py
+mpiexec -n 5 python3 anelastic_RB_fplane.py
 
 # python3 rayleigh_benard.py
 python3 merge.py raw_data/snapshots --cleanup
--- sim0.sh
+++ sim0.sh
@@ -3,6 +3,6 @@
 source /opt/Miniconda/miniconda37/etc/profile.d/conda.sh
 conda activate dedalus
 
-mpiexec -n 4  python3 anelastic_RB_fplane0.py
+mpiexec -n 5  python3 anelastic_RB_fplane0.py
 
 exit
