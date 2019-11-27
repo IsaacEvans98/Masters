9cebff98aab9d5ca8b85e80b115a1160dcf5096f
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2019-11-27 16:40:11 +0000

    sim0.sh

--- sim0.sh
+++ sim0.sh
@@ -5,4 +5,12 @@ conda activate dedalus
 
 mpiexec -n 5  python3 anelastic_RB_fplane0.py
 
+python3 merge.py raw_data2/snapshots --cleanup
+python3 merge.py raw_data2/analysis --cleanup
+python3 merge.py raw_data2/run_parameters --cleanup
+python3 merge_single0.py
+
+python3 plotting_snapshots0.py
+
+
 exit
