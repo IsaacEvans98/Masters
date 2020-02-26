6ca7accba2b2997903b0daa030cf369a3a4d59eb
Isaac Evans <isaac@Isaacs-MacBook-Air.local>
2020-02-26 13:22:22 +0000

    modified

--- Timestamp.py
+++ Timestamp.py
@@ -1,19 +1,3 @@
-import os
-import platform
-
-def creation_date():
-    """
-    Try to get the date that a file was created, falling back to when it was
-    last modified if that isn't possible.
-    See http://stackoverflow.com/a/39501288/1709587 for explanation.
-    """
-    if platform.system() == 'Windows':
-        return os.path.getctime()
-    else:
-        stat = os.stat()
-        try:
-            return stat.st_birthtime
-        except AttributeError:
-            # We're probably on Linux. No easy way to get creation dates here,
-            # so we'll settle for when its content was last modified.
-            return stat.st_mtime
+import os.path, time
+print("last modified: %s" % time.ctime(os.path.getmtime(sys.argv[1])))
+print("created: %s" % time.ctime(os.path.getctime(sys.argv[1])))
